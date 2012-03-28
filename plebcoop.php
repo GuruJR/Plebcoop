52badb296380f1d970bb1ccb3cc1f7f77cf12602
GuruJR <peeweeadd@gmail.com>
2012-03-28 07:44:49 -0400

    alpha.0.1.Build.5
    Basic flow

--- plebcoop.php
+++ plebcoop.php
@@ -1,5 +1,67 @@
-
 <?php
+/*
+ Version = 'alpha.0.1.Build.5';
+ 
+*/
+$Plop_salt = 'salt';
+/*     Sanitize Inputs  
+ return preg_replace("/[^A-Za-z0-9]/", "", $in);
+*/
+
+$Plop_action = ''.TxtOnlyStrict($_POST['V1']);   //action 
+$Plop_secret = ''.TxtOnlyStrict($_POST['V2']);   //secret 
+$Plop_msg = ''.TxtOnlyStrict($_POST['V3']);  //msg
+$Plop_dest = ''.TxtOnlyStrict($_POST['V4']);  //dest
+
+/*	Debug Area
+
+
+$Plop_action = 'd';
+$Plop_action = 'e';
+
+$Plop_secret = 'Really hard to guess phrase'
+
+$Plop_msg = array('1' => 'type','item2' => 'value1');
+$Plop_msg = 'string';
+
+$Plop_dest =
+*/
+
+
+
+
+
+
+
+
+
+/*    Flow 
+
+	        Give direction
+		to encrypted need		
+		FROM stringencrypted to jsonarray
+*/
+Switch($Plop_action){
+
+
+case 'e':
+$sendenc =  base64_encode(json_encode(enc($Plop_salt,$Plop_secret,$Plop_msg))); 
+echo $sendenc;
+break;
+
+case 'd':
+$plob = json_decode(base64_decode($Plop_msg),true);
+$out = Dcrpt($Plop_salt,$Plop_secret,$plob['iv'],$plob['Enc']);
+echo base64_encode(json_encode($out['load_decrypted']));
+break;
+
+default:
+
+}
+
+
+//Functions
+
 function Dcrpt($salt,$secret,$Base64iv,$Base64encrypted)
 {
 
@@ -48,12 +110,11 @@ function enc($salt,$secret,$unsecurarr){
   'Enc' => $encoded);
 
 }
-$test = array('1' => 'type','item2' => 'value1');
-$sendenc =  base64_encode(json_encode(enc('salt','secret',$test))); 
-$plob = json_decode(base64_decode($sendenc),true);
 
-$out= Dcrpt('salt','secret',$plob['iv'],$plob['Enc']);
+function TxtOnlyStrict($in){
+//return only txt and numbers
+ return preg_replace("/[^A-Za-z0-9]/", "", $in);
+}
 
-print_r($sendenc);
-print_r($out['load_decrypted']);
+
 ?>
