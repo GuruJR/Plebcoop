65205afdc1c63b1280e8e12d1d375f0d8ed4ec70
GuruJR <peeweeadd@gmail.com>
2012-03-29 07:14:00 -0400

    Working Build alpha.0.1.Build.6

--- plebcoop.php
+++ plebcoop.php
@@ -1,46 +1,62 @@
 <?php
+
+/*     Sanitize Inputs  
+
+*/
+
+$Plop_action = $_POST["v1"];  
+$Plop_secret = $_POST["v2"];   
+$Plop_msg = $_POST["v3"];  
+header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 /*
- Version = 'alpha.0.1.Build.5';
+ Version = 'alpha.0.1.Build.6';
  
 */
 $Plop_salt = 'salt';
-/*     Sanitize Inputs  
- return preg_replace("/[^A-Za-z0-9]/", "", $in);
-*/
 
-$Plop_action = ''.TxtOnlyStrict($_POST['V1']);   //action 
-$Plop_secret = ''.TxtOnlyStrict($_POST['V2']);   //secret 
-$Plop_msg = ''.TxtOnlyStrict($_POST['V3']);  //msg
-$Plop_dest = ''.TxtOnlyStrict($_POST['V4']);  //dest
+
 
 /*	Debug Area
-
+print_r($_POST);
+ return preg_replace("/[^A-Za-z0-9]/", "", $in); is maybe too aggresive for base64
+$Plop_dest = TxtOnlyStrict($_Post['v4']);  //dest
 
 $Plop_action = 'd';
 $Plop_action = 'e';
 
-$Plop_secret = 'Really hard to guess phrase'
+$Plop_secret = 'Really hard to guess phrase';
 
 $Plop_msg = array('1' => 'type','item2' => 'value1');
 $Plop_msg = 'string';
 
-$Plop_dest =
-*/
+$Plop_dest = '' ;
 
 
+$Plop_action = 'e';
+$Plop_msg = 'msg';
+$Plop_secret = 'key';
+$Plop_action = 'd';
+$Plop_msg = 'eyJpdiI6IlgxQnUydWlCaW5EQmxJQzk4QjNGb244cmZ6aWxXQTJUWTFISVc4dFBVcGM9IiwiRW5jIjoiU3dRRGRrZDdnRDJtTG12dzJ0NFJJU0JWYmpjbDRGb0ZKWjRHTDZqTDlIOD0ifQ==';
+$Plop_secret = 'key';
+
+2nd
+test unit
+woring
+$Plop_action = 'e';
+$Plop_msg = 'msg';
+$Plop_secret = 'key';
 
 
-
-
-
-
-
-/*    Flow 
+  Flow 
 
 	        Give direction
 		to encrypted need		
 		FROM stringencrypted to jsonarray
 */
+
+
+
 Switch($Plop_action){
 
 
@@ -52,7 +68,7 @@ break;
 case 'd':
 $plob = json_decode(base64_decode($Plop_msg),true);
 $out = Dcrpt($Plop_salt,$Plop_secret,$plob['iv'],$plob['Enc']);
-echo base64_encode(json_encode($out['load_decrypted']));
+echo $out;
 break;
 
 default:
@@ -61,7 +77,6 @@ default:
 
 
 //Functions
-
 function Dcrpt($salt,$secret,$Base64iv,$Base64encrypted)
 {
 
@@ -75,8 +90,8 @@ function Dcrpt($salt,$secret,$Base64iv,$Base64encrypted)
     $decrypted = trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $k, $decoded, MCRYPT_MODE_CBC, $iv));
 
     // Deserializing & loading
-    $unser = json_decode($decrypted,true);
-  return array('load_decrypted' => $unser);
+    $unser = $decrypted;
+  return  $unser ;
 }
 
 function enc($salt,$secret,$unsecurarr){
@@ -92,7 +107,7 @@ function enc($salt,$secret,$unsecurarr){
    //To create an  advanced Full key Be low
    //$k_size = mcrypt_get_key_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC)
    //$k = mcrypt_create_iv( mcrypt_get_key_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC);
-    // Encrypting
+   // Encrypting
 
     $encrypted = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $k, $serialized, MCRYPT_MODE_CBC, $iv);
 
@@ -108,12 +123,13 @@ function enc($salt,$secret,$unsecurarr){
   //  'Returned_Encr' => $encrypted,
   */
   'Enc' => $encoded);
-
 }
 
 function TxtOnlyStrict($in){
 //return only txt and numbers
- return preg_replace("/[^A-Za-z0-9]/", "", $in);
+ // preg_replace("/[^A-Za-z0-9\=]/", "", $in);
+preg_replace("/[^A-Za-z0-9\=]/", "", $in);
+return $inutfted;
 }
 
 
